1eba16133ab96a3c3d88904e2eeaa49ec0e5b706
Brock <1540901+brocksexton@users.noreply.github.com>
2025-12-30 19:20:25 -0500

    Initial Commit

---
+++ Brockbuster/BrockbusterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrockbusterApp.swift
+//  Brockbuster
+//
+//  Created by Brock Sexton on 2025-12-30.
+//
+
+import SwiftUI
+
+@main
+struct BrockbusterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brockbuster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Brockbuster
+//
+//  Created by Brock Sexton on 2025-12-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
